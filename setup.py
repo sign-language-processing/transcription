710ffadd1983a65096c11266337c05b452f4aed7
Amit Moryossef <amitmoryossef@gmail.com>
2022-03-27 13:07:32 +0200

    chore(): add missing requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as fr:
+    requirements = fr.read().splitlines()
 
 setup(
     name="sign_text_to_pose",
@@ -15,8 +17,8 @@ setup(
     author_email="amitmoryossef@gmail.com",
     url="https://github.com/sign/text-to-pose",
     keywords=["Sign Language"],
-    install_requires=[],
+    install_requires=requirements,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    classifiers=["Programming Language :: Python :: 3.8",],
+    classifiers=["Programming Language :: Python :: 3.8"],
 )
