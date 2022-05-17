2115a3bdeb1257caa9e928e22cf8e6f5d8966f90
Amit Moryossef <amitmoryossef@gmail.com>
2022-05-17 17:39:58 +0200

    test(tokenizer): add single token expected token id test

--- text_to_pose/tests/tokenizer_test.py
+++ text_to_pose/tests/tokenizer_test.py
@@ -6,10 +6,17 @@ from ...shared.tokenizers import HamNoSysTokenizer
 
 
 class TokenizerTestCase(unittest.TestCase):
+    def test_expected_token_id(self):
+        tokenizer = HamNoSysTokenizer()
+        tokenized = tokenizer(["\ue000\ue071"])
+        self.assertEqual(tokenized['tokens_ids'][0][0], 1)
+        self.assertEqual(tokenized['tokens_ids'][0][1], 13)
+        self.assertEqual(tokenized['tokens_ids'][0][2], 97)
+
     def test_multiple_sentence(self):
         expected = {
-            'tokens_ids': torch.tensor([[1, 11, 95, 0, 0, 0, 0, 0, 0],
-                                        [1, 11, 95, 28, 40, 56, 118, 176, 190]], dtype=torch.int32),
+            'tokens_ids': torch.tensor([[1, 13, 97, 0, 0, 0, 0, 0, 0],
+                                        [1, 13,  97,  30,  42,  58, 120, 178, 192]], dtype=torch.int32),
             'positions': torch.tensor([[0, 1, 2, 0, 0, 0, 0, 0, 0],
                                        [0, 1, 2, 3, 4, 5, 6, 7, 8]], dtype=torch.int32),
             'attention_mask': torch.tensor([[False, False, False, True, True, True, True, True, True],
